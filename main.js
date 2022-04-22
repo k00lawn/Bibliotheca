3ad20d4aa06210a9a0c2174b84b0b80b67979ac5
Koolawn Alseri <k00lawn@gmail.com>
2022-04-22 12:45:29 +0530

    book preview feature added

--- main.js
+++ main.js
@@ -205,6 +205,7 @@ function createBook (book) {
 
     cover.addEventListener('click', () => {
         openCanvas()
+        readBook(coverTitle)
     })
 
     cover.addEventListener("dblclick", () => {
@@ -215,6 +216,98 @@ function createBook (book) {
 
     
  
+}
+
+//Read Book 
+
+const readBook = bookName => {
+    const filename = null;
+    const url = '../docs/pdf.pdf';
+
+let pdfDoc = null,
+    pageNum = 1,
+    pageIsRendering = false,
+    pageNumIsPending = null;
+
+const scale = 1.5,
+    canvas = document.querySelector('#pdf-render'),
+    ctx = canvas.getContext('2d');
+
+// Render the page
+const renderPage = num => {
+    pageIsRendering = true;
+
+    pdfDoc.getPage(num).then(page => {
+        const viewport = page.getViewport({ scale })
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        const renderCtx = {
+            canvasContext: ctx,
+            viewport
+        }
+
+        page.render(renderCtx).promise.then(() => {
+            pageIsRendering = false;
+
+            if(pageNumIsPending !== null) {
+                renderPage(pageNumIsPending);
+                pageNumIsPending = null;
+            }
+        })
+
+        document.querySelector('#page-num').textContent = num;
+    })
+};
+
+const queueRenderPage = num => {
+    if(pageIsRendering) {
+        pageNumIsPending = num; 
+    } else {
+        renderPage(num)
+    }
+}
+
+const showPrevPage = () => {
+    if(pageNum <= 1) {
+        return;
+    }
+
+    pageNum--;
+    queueRenderPage(pageNum)
+}
+
+const showNextPage = () => {
+    if(pageNum >= pdfDoc.numPages) {
+        return;
+    }
+
+    pageNum++;
+    queueRenderPage(pageNum)
+}
+
+pdfjsLib.getDocument(url).promise.then(pdfDoc_ => {
+    pdfDoc = pdfDoc_;
+    console.log(pdfDoc);
+    document.querySelector('#page-count').textContent = pdfDoc.numPages;
+
+    renderPage(pageNum)
+})
+    .catch(err => {
+        const div = document.createElement('div');
+        div.className = 'error';
+        div.appendChild(document.createTextNode(err.message));
+        document.querySelector('body').insertBefore(div, canvas)
+
+        document.querySelector('.top-bar').style.display = 'none';
+    })
+
+//Button Events
+
+document.querySelector('#prev-page').addEventListener('click', showPrevPage)
+document.querySelector('#next-page').addEventListener('click', showNextPage)
+
+
 }
 
 //Save function to save books
