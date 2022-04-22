40fb414371339c22ad12c93933df05c6f6af8234
Koolawn Alseri <k00lawn@gmail.com>
2022-04-22 16:17:43 +0530

    pdf uploading is successful

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require('express')
 const path = require('path')
+const multer = require('multer')
 
 const app = express();
+
+const fileStorageEngine = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./public/docs")
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({storage: fileStorageEngine})
+
+app.post('/single', upload.single('pdf'), (req, res) => {
+    console.log(req.file)
+    res.send('PDF file uploaded successfully')
+})
 
 app.use(express.static(path.join(__dirname, 'public')))
 
